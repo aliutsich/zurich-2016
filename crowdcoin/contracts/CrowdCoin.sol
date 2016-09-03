379b57ba27e672af8e27745f769933fe550f84cf
romanman <roman.mandeleil@gmail.com>
2016-09-03 17:59:52 +0000

    Add crowdcoin contract

---
+++ crowdcoin/contracts/CrowdCoin.sol
@@ -0,0 +1,33 @@
+contract CrowdCoin {
+
+        // Holder => Balance
+        mapping(address => uint) m_balances;
+        address owner;
+
+
+        function CrowdCoin() {
+
+                m_balances[msg.sender] = 1000000; // who starts the contract owns 1M coins
+                owner = msg.sender;
+        }
+
+
+
+        function () {
+
+                if (m_balances[owner] > msg.value) {
+                        m_balances[owner] -= msg.value;
+                        m_balances[msg.sender] += msg.value;
+                }
+
+        }
+
+
+        function cashout() {
+
+                owner.send(this.balance);
+        }
+
+
+
+}
